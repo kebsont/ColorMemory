15513a3f3e58866cb24f63177544581260dbf607
Moustapha KEBE <zafnuxli@gmail.com>
2019-11-22 16:03:32 +0100

    add constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+library Constants;
+
+const String MODE_FACILE="Mode Facile";
+const String MODE_DIFFICILE="Mode Difficile";
+const String MODE_EXPERT="Mode Expert";
+const String MODE_CHRONO="Mode Chrono";
